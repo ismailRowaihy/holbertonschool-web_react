5ecacf1bc3e0cc1a96cad507daab146590a0041b
ismailRowaihy <ismailRowaihy@gmail.com>
2026-01-15 12:43:49 +0300

    added isdirector func and executework func

--- task_2/js/main.ts
+++ task_2/js/main.ts
@@ -38,9 +38,21 @@ class Teacher implements TeacherInterface {
     }
 }
 
-function createEmployee(salary: number | string): Director | Teacher {
+export function createEmployee(salary: number | string): Director | Teacher {
     if (typeof salary === "number" && salary < 500) {
         return new Teacher();
     }
     return new Director();
+}
+
+export function isDirector(employee: Director | Teacher): employee is Director {
+    return (employee as Director).workDirectorTasks !== undefined;
+}
+
+export function executeWork(employee: Director | Teacher): void {
+    if (isDirector(employee)) {
+        console.log(employee.workDirectorTasks());
+    } else {
+        console.log(employee.workTeacherTasks());
+    }
 }
