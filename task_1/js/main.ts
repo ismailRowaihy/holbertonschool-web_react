3bd108ddb01abd1a5e5e3211b4cf18a643e16760
ismailRowaihy <ismailRowaihy@gmail.com>
2026-01-15 12:27:51 +0300

    added print teacher func

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -12,10 +12,10 @@ interface Directors extends Teacher {
 }
 
 const teacher3: Teacher = {
-    firstName: 'some',
+    firstName: 'John',
     fullTimeEmployee: false,
-    lastName: 'dude',
-    location: 'here',
+    lastName: 'Doe',
+    location: 'London',
     contract: false,
 };
 
@@ -27,5 +27,14 @@ const director1: Directors = {
     numberOfReports: 17,
 };
 
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+export const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+    return `${firstName.charAt(0)}. ${lastName}`;
+};
+
 console.log(director1);
 console.log(teacher3);
+console.log(printTeacher("John", "Doe"));
